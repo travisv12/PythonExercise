702f1e6e22bd87491c1cf92406be3312d702f370
trung <trungvu1279@gmail.com>
2023-09-05 16:16:55 +0300

    homework

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ import random
 d = int(input("how many dots: "))
 cp = 0
 sp = 0
-for i in range(d**2):
+for i in range(d):
     randx = random.uniform(-1,1)
     randy = random.uniform(-1,1)
     distant = randx**2 + randy**2
@@ -154,10 +154,69 @@ for i in range(d**2):
     pi = 4*cp/sp
 print("The estimated value of pi is: ",pi)
 
+print("4.6")
+import random
+d = int(input("how many dots: "))
+cp = 0
+sp = 0
+d1 = 0
+while d1 < d:
+    randx = random.uniform(-1,1)
+    randy = random.uniform(-1,1)
+    distant = randx**2 + randy**2
+    if distant <=1:
+        cp = cp + 1
+    d1 = d1 + 1
+    sp = sp + 1
+    pi = 4*cp/sp
+print("The estimated value of pi is: ",pi)
 
+print("5.1")
+import random
+rolls = []
+roll = int(input("how many rolls: "))
 
+for i in range(roll):
+    n = random.randint(1,6)
+    rolls.append(n)
+print("the rolls in order are: ",rolls)
+print("the sum of rolls are:",sum(rolls))
 
+print("5.2")
+no = input("enter a number: ")
+list =[]
+while True:
+    if no == "":
+        break
+    no = float(no)
+    list.append(no)
+    no = input("enter a number: ")
+reverse = sorted(list, reverse=True)
+print("top 5 biggest numbers are: ",reverse[:5])
 
+print("5.3")
+no = int(input("enter an interger: "))
+o = 0
+for i in range(1,no):
+    if no%i==0:
+        o=o+1
+    else:
+        o = o
+if o == 1:
+    print("this is a prime number")
+else:
+    print("this is not a prime number")
 
-
+print("5.4")
+cities = []
+city = input("add a city name: ")
+n=1
+while n<5:
+    cities.append(city)
+    city = input("add a city name: ")
+    n = n + 1
+cities.append(city)
+print(cities)
+for index,i in enumerate(cities, start=1):
+    print(f"{index}. {i}")
 
